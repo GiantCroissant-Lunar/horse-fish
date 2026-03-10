ce08160175edc27796a93b1217aae4caa6b2f29f
ApprenticeGC <apprentice@giantcroissant.com>
2026-03-10 08:35:12 +0800

    chore: auto-fix lint

---
+++ src/horse_fish/utils/hello.py
@@ -0,0 +1,10 @@
+"""Hello world utility."""
+
+
+def hello_world() -> str:
+    """Return the string 'hello world'.
+
+    Returns:
+        str: The greeting 'hello world'.
+    """
+    return "hello world"
